5f777b5f3c173ea912c2eab62a9153e23e909cec
Linda <lindaallinson123@gmail.com>
2022-03-24 23:24:23 +0000

    Removing list of operators so that user input creates the dict instead of creating a list and converting to a dict

--- ext_calc_module.py
+++ ext_calc_module.py
@@ -1,4 +1,6 @@
 import operator
+
+
 def welcome_calc():
     print("""
     Welcome to Calculator! Enter a first set of numbers and then a second set. Then enter as many 
@@ -10,9 +12,10 @@ def welcome_calc():
     Enjoy!
     """)
 
+
 def get_list_of_numbers_from_user():
-    num=0
-    num_list=[]
+    num = "0"
+    num_list = []
     while num != "f":
         num = input("Please enter a number: ")
         if num != "f":
@@ -23,8 +26,9 @@ def get_list_of_numbers_from_user():
                 print("Only numbers will work!  ")
     return num_list
 
+
 def get_list_of_operators_from_user():
-    op = 0
+    op = "0"
     operator_dict = {}
     while op != "f":
         op = input("Please enter an operator: ")
@@ -42,7 +46,7 @@ def get_list_of_operators_from_user():
     return operator_dict
 
 
-def print_result_and_return_file_content_to_save(first_nums,second_nums,operators):
+def print_result_and_return_file_content_to_save(first_nums, second_nums, operators):
     file_list = []
     print("\n----------------------------------------------------------\n")
     print("\nAll possible answers are: \n")
@@ -50,32 +54,35 @@ def print_result_and_return_file_content_to_save(first_nums,second_nums,operator
         for j in second_nums:
             for k, v in operators.items():
                 if j == 0 and k == "/":
-                    calcVar1 = f"Cannot perform {i} {k} {j} because you cannot divide by zero."
-                    file_list.append(calcVar1)
-                    print(calcVar1)
+                    calc_var_1 = f"Cannot perform {i} {k} {j} because you cannot divide by zero."
+                    file_list.append(calc_var_1)
+                    print(calc_var_1)
                     continue
                 else:
-                    calcVar2 = f"{i}{k}{j} =  {v(i, j)}"
-                    file_list.append(calcVar2 + "\n")
-                    print(calcVar2)
+                    calc_var_2 = f"{i}{k}{j} =  {v(i, j)}"
+                    file_list.append(calc_var_2 + "\n")
+                    print(calc_var_2)
                     continue
     return file_list
 
 
 def calc_save_result(content_to_be_saved):
-    saveToFile = ""
-    userFileName = 0
-    while userFileName != "q":
-        userFileName = input('Press q to quit or if you would like to save your work insert a file name. It must start with a letter, only use letters and numbers, and be no more than 15 characters long: ')
-        if all([i.isalpha() or i.isdigit() for i in userFileName]) and userFileName[0].isalpha() and len(
-                userFileName) <= 15 and userFileName != "q":
+    user_file_name = "0"
+    while user_file_name != "q":
+        user_file_name = input("""
+                                Press q to quit or if you would like to save your work insert a file name. It must
+                                start with a letter, only use letters and numbers, and be no more than 15 characters
+                                long: 
+                               """)
+        if all([i.isalpha() or i.isdigit() for i in user_file_name]) and user_file_name[0].isalpha() and len(
+                user_file_name) <= 15 and user_file_name != "q":
             try:
-                fileCreated = open("%s.txt" % userFileName, "x")
-                fileCreated.writelines(content_to_be_saved)
-                fileCreated.close()
+                file_created = open("%s.txt" % user_file_name, "x")
+                file_created.writelines(content_to_be_saved)
+                file_created.close()
                 break
             except FileExistsError:
                 print("That file name exists already. Try again or press 'q' to quit.")
         else:
-            if userFileName != "q":
+            if user_file_name != "q":
                 print("Try again or press 'q' to quit")
